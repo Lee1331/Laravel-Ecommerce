39c83765891f239cfdd741c4b76a140deee5473b
Lee <lee1331@hotmail.co.uk>
2019-10-20 15:09:12 +0100

    Added RoleTableSeeder and  Role-User relationships

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    //Relationships
+    public function users(){
+        return $this->belongsToMany('App\User');
+    }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Role;
+
 class UserTableSeeder extends Seeder
 {
     /**
@@ -11,13 +14,34 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                'name' => 'Test',
-                'email' => 'test@gmail.com',
-                'password' => bcrypt('secret'),
-                'email_verified_at' => now(),
-            ]
+        User::truncate();
+        DB::table('role_user')->truncate();
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $authorRole = Role::where('name','author')->first();
+        $userRole = Role::where('name', 'user')->first();
+
+        $admin = User::create([
+            'name' => 'Lee',
+            'email' => 'lee1331@hotmail.co.uk',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => now(),
         ]);
+        $author = User::create([
+            'name' => 'Jeaves',
+            'email' => 'jeaves@gmail.com',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => now(),
+        ]);
+        $user = User::create([
+            'name' => 'Test',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => now(),
+        ]);
+
+        $admin->roles()->attach($adminRole);
+        $author->roles()->attach($authorRole);
+        $user->roles()->attach($userRole);
     }
 }
